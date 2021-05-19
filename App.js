6946dfad2d0476b0e10f8bfc26fa975bfac58242
Cedric Smith <cedricsmith@Cedrics-MacBook-Air.local>
2021-05-19 12:45:56 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,53 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
+
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKQvPzi4XqX2Os-G7PUH3MhQ3KCBMTTQg",
+  authDomain: "mealstogo-dbc12.firebaseapp.com",
+  projectId: "mealstogo-dbc12",
+  storageBucket: "mealstogo-dbc12.appspot.com",
+  messagingSenderId: "981355692234",
+  appId: "1:981355692234:web:e717be91831052b0e5b433",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
